28d07cb6a74edc66717e47d17c1c1d7ac8b34d9b
Bruce Manning <themoose3@gmail.com>
2025-05-06 14:22:26 -0400

    Add existing project files to git

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { createClient } = require('@libsql/client')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express()
+const port = 3000
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const turso = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+// Example: GET endpoint to fetch data
+app.get('/drinks/specials', async (_, res) => {
+  const result = await turso.execute("SELECT name, id FROM drinks WHERE currentSpecial = true ORDER BY name ASC");
+  res.json(result);
+});
+
+app.get('/drinks', async (_, res) => {
+  const result = await turso.execute("SELECT name, id FROM drinks ORDER BY name ASC");
+  res.json(result);
+});
+
+app.get('/drinks/:id', async (req, res) => {
+  const id = req.params.id;
+  const result = await turso.execute(`SELECT * FROM drinks WHERE id = ${id}`);
+  res.json(result);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
